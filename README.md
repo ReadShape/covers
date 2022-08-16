16bc0b456317102e97eeaeb705db2ba77c8f5475
Toms Burgmanis <toms.burgmanis@gmail.com>
2022-08-16 23:20:36 +0200

    Initial commit (via bun create)

---
+++ README.md
@@ -0,0 +1,24 @@
+# React with Bun runtime
+
+This is a React project bootstrapped with [bun](https://bun.sh/).
+
+## Getting Started
+
+### Cloning the repo
+
+```sh
+bun create react ./react-bun-app
+```
+
+### Development
+
+First, run the development server.
+
+```
+bun dev
+```
+
+Open http://localhost:3000 with your browser to see the result.
+
+You can start editing the page by modifying src/App.jsx. The page auto-updates as you edit the file.
+
---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>Welcome to React!</h3>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
